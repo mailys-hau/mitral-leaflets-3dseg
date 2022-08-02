6c4e1237684f6d3fe19246660bef673c14d97f5d
mailys-hau <27966914+mailys-hau@users.noreply.github.com>
2022-08-02 16:13:27 +0200

    main: helper for config file handling

---
+++ src/utils/__init__.py
@@ -0,0 +1,5 @@
+from utils.misc import InclusiveLoader, rec_update
+
+
+
+InclusiveLoader.add_constructor("!include", InclusiveLoader.include)
---
+++ src/utils/misc.py
@@ -0,0 +1,26 @@
+import yaml
+
+from collections.abc import Mapping
+from pathlib import Path
+
+
+
+class InclusiveLoader(yaml.SafeLoader):
+    """ Allow use of `!include filename.yml` statement in yaml file """
+    def __init__(self, stream):
+        super(InclusiveLoader, self).__init__(stream)
+
+    def include(self, node):
+        fname = Path(self.construct_scalar(node)).expanduser().resolve()
+        with open(fname, 'r') as fd:
+            return yaml.load(fd, InclusiveLoader)
+
+
+def rec_update(d, u):
+    """ Recursively update dict of any depth """
+    for k, v in u.items():
+        if isinstance(v, Mapping):
+            d[k] = rec_update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
