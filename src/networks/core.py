699eb661df8ab0af9d3d257a8af8b03974142424
mailys-hau <27966924+mailys-hau@users.noreply.github.com>
2022-11-03 17:23:51 +0100

    net: fix: metrics accept long or float + code refactored

--- src/networks/core.py
+++ src/networks/core.py
@@ -1,5 +1,6 @@
 import inspect as ispc
 import pytorch_lightning as pl
+import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchmetrics
@@ -17,20 +18,29 @@ class EnhancedLightningModule(pl.LightningModule):
 
     def _init_metrics(self, metrics):
         all_metrics = dict(ispc.getmembers(torchmetrics, ispc.isclass))
+        # We use `nn.ModuleDict` to always be on proper device and such
+        self.metrics = nn.ModuleDict()
         def build_metric(name, *args, **kwargs):
             return all_metrics[name](*args, **kwargs)
-        self.train_metrics = nn.ModuleDict({
-            m["display_name"]: build_metric(m["name"], *m.get("args", []), **m.get("kwargs", {}))
-            for m in metrics })
-        self.val_metrics = nn.ModuleDict({
-            f"v_{m['display_name']}": build_metric(m["name"], *m.get("args", []), **m.get("kwargs", {}))
-            for m in metrics })
-        self.test_metrics = nn.ModuleDict({
-            m["display_name"]: build_metric(m["name"], *m.get("args", []), **m.get("kwargs", {}))
-            for m in metrics })
+        self.metrics["mtrain"] = nn.ModuleDict({
+            m["display_name"]: build_metric(m["name"],
+                                            *m.get("args", []),
+                                            **m.get("kwargs", {}))
+                for m in metrics })
+        self.metrics["mval"] = nn.ModuleDict({
+            f"v_{m['display_name']}": build_metric(m["name"],
+                                                   *m.get("args", []),
+                                                   **m.get("kwargs", {}))
+                for m in metrics })
+        self.metrics["mtest"] = nn.ModuleDict({
+            m["display_name"]: build_metric(m["name"],
+                                            *m.get("args", []),
+                                            **m.get("kwargs", {}))
+                for m in metrics })
 
 
     def get_progress_bar_dict(self):
+        #FIXME: Deprecated
         # Don't show version number on progressbar
         items = super().get_progress_bar_dict()
         items.pop("v_num", None)
@@ -44,14 +54,23 @@ class EnhancedLightningModule(pl.LightningModule):
         errs = self.loss(self.preds, y)
         return errs
 
+    def _update_metrics(self, y, mode="train", log=True):
+        # Not sure why but key "train" isn't allowed for an `nn.ModuleDict`
+        metrics = self.metrics[f"m{mode}"]
+        # For classification, most metrics only accept logit target
+        int_y = y.to(torch.long)
+        for m in metrics.values():
+            try:
+                m(self.preds, int_y)
+            except ValueError:
+                m(self.preds, y) # Rare cases where float is needed
+        self.log_dict(metrics, on_epoch=True)
+
 
     def training_step(self, batch, batch_idx):
         _, y = batch
         errs = self._step(batch, batch_idx)
-        # Update metrics
-        for m in self.train_metrics.values():
-            m(self.preds, y)
-        self.log_dict(self.train_metrics, on_epoch=True)
+        self._update_metrics(y)
         if isinstance(errs, dict): # Used in VAE for example
             self.log_dict(errs, prog_bar=True, on_step=True, on_epoch=True)
             return errs["loss"]
@@ -61,10 +80,7 @@ class EnhancedLightningModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         _, y = batch
         errs = self._step(batch, batch_idx)
-        # Update metrics
-        for m in self.val_metrics.values():
-            m(self.preds, y)
-        self.log_dict(self.val_metrics, on_epoch=True)
+        self._update_metrics(y, "val")
         if isinstance(errs, dict): # Used in VAE for example
             errs = {f"v_{k}": v for k, v in errs.items()}
             self.log_dict(errs, prog_bar=True, on_epoch=True)
@@ -75,10 +91,7 @@ class EnhancedLightningModule(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         _, y = batch
         errs = self._step(batch, batch_idx)
-        # Update metrics
-        for m in self.test_metrics.values():
-            m(self.preds, y)
-        self.log_dict(self.test_metrics, on_epoch=True)
+        self._update_metrics(y, "test")
         if isinstance(errs, dict): # Used in VAE for example
             self.log_dict(errs, prog_bar=True, on_epoch=True)
             return errs["loss"]
