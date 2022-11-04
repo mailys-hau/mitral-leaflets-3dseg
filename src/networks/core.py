7a35c439ef683b65ef572d641e79e2baae3ff88f
mailys-hau <27966924+mailys-hau@users.noreply.github.com>
2022-11-04 12:00:05 +0100

    net: fix: refactor error logging

--- src/networks/core.py
+++ src/networks/core.py
@@ -56,39 +56,36 @@ class EnhancedLightningModule(pl.LightningModule):
                 m(self.preds, int_y)
             except ValueError:
                 m(self.preds, y) # Rare cases where float is needed
+        #FIXME: Load by steps for test_step
         self.log_dict(metrics, on_epoch=True)
+
+    def _log_errs(self, errs, name="loss", on_step=False):
+        if isinstance(errs, dict): # Used in VAE for example
+            if 'v' in name:
+                errs = {f"v_{k}": v for k, v in errs.items()}
+            self.log_dict(errs, prog_bar=True, on_step=on_step, on_epoch=True)
+            return errs[name]
+        self.log(name, errs, prog_bar=True, on_step=on_step, on_epoch=True)
+        return errs if name == "loss" else {"v_loss": errs}
 
 
     def training_step(self, batch, batch_idx):
         _, y = batch
         errs = self._step(batch, batch_idx)
         self._update_metrics(y)
-        if isinstance(errs, dict): # Used in VAE for example
-            self.log_dict(errs, prog_bar=True, on_step=True, on_epoch=True)
-            return errs["loss"]
-        self.log("loss", errs, prog_bar=True, on_step=True, on_epoch=True)
-        return errs
+        return self._log_errs(errs, on_step=True)
 
     def validation_step(self, batch, batch_idx):
         _, y = batch
         errs = self._step(batch, batch_idx)
         self._update_metrics(y, "val")
-        if isinstance(errs, dict): # Used in VAE for example
-            errs = {f"v_{k}": v for k, v in errs.items()}
-            self.log_dict(errs, prog_bar=True, on_epoch=True)
-            return errs["v_loss"]
-        self.log("v_loss", errs, prog_bar=True, on_epoch=True)
-        return {"v_loss": errs}
+        return self._log_errs(errs, name="v_loss")
 
     def test_step(self, batch, batch_idx):
         _, y = batch
         errs = self._step(batch, batch_idx)
         self._update_metrics(y, "test")
-        if isinstance(errs, dict): # Used in VAE for example
-            self.log_dict(errs, prog_bar=True, on_epoch=True)
-            return errs["loss"]
-        self.log("loss", errs, prog_bar=True, on_epoch=True)
-        return errs
+        return self._log_errs(errs)
 
     def predict(self, batch, batch_idx, dataloader_idx=None):
         errs = self._step(batch, batch_idx)
