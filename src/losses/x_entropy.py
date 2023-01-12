03656da0825a0ea82a22798268d5a2b697db1a15
mailys-hau <27966924+mailys-hau@users.noreply.github.com>
2023-01-12 14:40:50 +0100

    loss: fix: None weight handling

--- src/losses/x_entropy.py
+++ src/losses/x_entropy.py
@@ -7,6 +7,6 @@ class XEntropyLoss(nn.CrossEntropyLoss):
     def __init__(self, weight=None, size_average=None, ignore_index=-100,
                  reduce=None, reduction="mean", label_smoothing=0):
         # Wrapper to accept list as weight parameter
-        weight = torch.Tensor(weight)
+        weight = torch.Tensor(weight) if weight is not None else weight
         super(XEntropyLoss, self).__init__(weight, size_average, ignore_index,
                                            reduce, reduction, label_smoothing)
