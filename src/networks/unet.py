1809f2ef8ef778c0ccb2c701508bc6ec41bbe5f7
mailys-hau <27966924+mailys-hau@users.noreply.github.com>
2023-01-25 11:57:20 +0100

    net: fix: remove outdated parameters in UNets

--- src/networks/unet.py
+++ src/networks/unet.py
@@ -15,15 +15,14 @@ class BasicUNet(EnhancedLightningModule):
                  features=(32, 32, 64, 128, 256, 32),
                  act=("LeakyReLU", {"negative_slope": 0.1, "inplace": True}),
                  norm=("instance", {"affine": True}), bias=True, dropout=0.0,
-                 upsample="deconv", dimensions=None,
+                 upsample="deconv",
                  # Added personal arguments
                  final_activation=nn.Sigmoid()):
         super(BasicUNet, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
         self.model = mnn.BasicUNet(
                 spatial_dims=spatial_dims, in_channels=in_channels,
                 out_channels=out_channels, features=features, act=act,
-                norm=norm, bias=bias, dropout=dropout, upsample=upsample,
-                dimensions=dimensions
+                norm=norm, bias=bias, dropout=dropout, upsample=upsample
                 )
         self.final_activation = final_activation
 
@@ -40,7 +39,7 @@ class UNet(EnhancedLightningModule):
                  spatial_dims=3, in_channels=1, out_channels=1,
                  channels=(8, 16, 32), strides=(2, 2), kernel_size=3,
                  up_kernel_size=3, num_res_units=0, act="PRELU", norm="INSTANCE",
-                 dropout=0, bias=True, adn_ordering="NDA", dimensions=None,
+                 dropout=0, bias=True, adn_ordering="NDA",
                  # Specific arguments
                  final_activation=nn.Sigmoid()):
         super(UNet, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
@@ -48,8 +47,7 @@ class UNet(EnhancedLightningModule):
                 spatial_dims, in_channels, out_channels, channels, strides,
                 kernel_size=kernel_size, up_kernel_size=up_kernel_size,
                 num_res_units=num_res_units, act=act, norm=norm,
-                dropout=dropout, bias=bias, adn_ordering=adn_ordering,
-                dimensions=dimensions
+                dropout=dropout, bias=bias, adn_ordering=adn_ordering
                 )
         self.final_activation = final_activation
 
