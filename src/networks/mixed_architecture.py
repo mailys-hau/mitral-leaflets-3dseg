net: fix: use softmax as final activation

src/networks/mixed_architecture.py
@@ -19,7 +19,7 @@ class SwinUNETR(EnhancedLightningModule):
                  loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
                  metrics=[],
                  # Added personal parameters
-                 final_activation=nn.Sigmoid()):
+                 final_activation=nn.Softmax(dim=1)):
         super(SwinUNETR, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
         self.model = mnn.SwinUNETR(
                 img_size, in_channels, out_channels, depths=depths,
@@ -47,7 +47,7 @@ class UNETR(EnhancedLightningModule):
                  loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
                  metrics=[],
                  # Added personal parameters
-                 final_activation=nn.Sigmoid()):
+                 final_activation=nn.Softmax(dim=1)):
         super(UNETR, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
         self.model = mnn.UNETR(
                 in_channels, out_channels, img_size, feature_size=feature_size,

src/networks/unet.py
@@ -17,7 +17,7 @@ class BasicUNet(EnhancedLightningModule):
                  norm=("instance", {"affine": True}), bias=True, dropout=0.0,
                  upsample="deconv",
                  # Added personal arguments
-                 final_activation=nn.Sigmoid()):
+                 final_activation=nn.Softmax(dim=1)):
         super(BasicUNet, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
         self.model = mnn.BasicUNet(
                 spatial_dims=spatial_dims, in_channels=in_channels,
@@ -41,7 +41,7 @@ class UNet(EnhancedLightningModule):
                  up_kernel_size=3, num_res_units=0, act="PRELU", norm="INSTANCE",
                  dropout=0, bias=True, adn_ordering="NDA",
                  # Specific arguments
-                 final_activation=nn.Sigmoid()):
+                 final_activation=nn.Softmax(dim=1)):
         super(UNet, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
         self.model = mnn.UNet(
                 spatial_dims, in_channels, out_channels, channels, strides,