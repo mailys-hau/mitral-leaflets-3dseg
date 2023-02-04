a04b921688728b78cd1e865807bc150429f615aa
mailys-hau <27966924+mailys-hau@users.noreply.github.com>
2023-02-04 18:53:08 +0100

    net: add fill holes post process + log acc per classes

--- src/networks/core.py
+++ src/networks/core.py
@@ -1,4 +1,5 @@
 import inspect as ispc
+import monai.transforms as mtr
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
@@ -17,6 +18,8 @@ class EnhancedLightningModule(pl.LightningModule):
         self.optimizer_config = optimizer
         self.lr_scheduler = lr_scheduler
         self._init_metrics(metrics)
+        self.post_process = None # Have to be initalize later
+
 
     def _init_metrics(self, metrics):
         all_metrics = dict(ispc.getmembers(torchmetrics, ispc.isclass))
@@ -39,23 +42,36 @@ class EnhancedLightningModule(pl.LightningModule):
 
 
     def _step(self, batch, batch_idx):
+        if self.post_process is None:
+            keep_labels = list(range(1, self.model.out_channels))
+            #FIXME: Make this deactivable from config file
+            # Don't process background
+            self.post_process = mtr.FillHoles(keep_labels)
         x, y = batch
         #FIXME: Patch for multi-inheritance
-        self.preds = self.forward(x)
-        errs = self.loss(self.preds, y)
+        out = self.forward(x)
+        errs = self.loss(out, y)
+        preds = []
+        for elt in out: # Post process need to be done element wise
+            preds.append(self.post_process(elt.squeeze()))
+        self.preds = torch.stack(preds)
         return errs
 
-    def _update_metrics(self, y, mode="train", log=True):
+    def _update_metrics(self, y, mode="train"):
+        #FIXME: Ever heard of `MetricCollection`?
         # Not sure why but key "train" isn't allowed for an `nn.ModuleDict`
         metrics = self.metrics[f"m{mode}"]
-        for m in metrics.values():
+        for k, m in metrics.items():
             try:
-                m(self.preds, y)
+                val = m(self.preds, y)
             except ValueError as err:
                 # Some metrics need int to compute, e.g. Dice
-                m(self.preds, y.to(torch.bool))
-        #FIXME: Load by steps for test_step
-        self.log_dict(metrics, on_epoch=True)
+                val = m(self.preds, y.to(torch.bool))
+            #FIXME: Load by steps for test
+            if val.numel() > 1:
+                self.log_dict({f"{k}/{i}": val[i] for i in range(len(val))}, on_epoch=True)
+            else:
+                self.log_dict({k: val}, on_epoch=True)
 
     def _log_errs(self, errs, name="loss", on_step=False):
         if isinstance(errs, dict): # Used in VAE for example
@@ -71,7 +87,8 @@ class EnhancedLightningModule(pl.LightningModule):
         _, y = batch
         errs = self._step(batch, batch_idx)
         self._update_metrics(y)
-        return self._log_errs(errs, on_step=True)
+        out = self._log_errs(errs, on_step=True)
+        return out
 
     def validation_step(self, batch, batch_idx):
         _, y = batch
