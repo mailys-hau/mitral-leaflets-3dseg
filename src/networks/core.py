net: fix: rm softmax before loss + pass all kwargs to pl
- Softmax baked in torch CrossEntropy, so don't compute it before
- Update the argument passing according to changes in
EnhancedLightningModule

src/networks/core.py
@@ -12,13 +12,15 @@ from utils import LinearCosineLR, MONAI_METRICS
 
 class EnhancedLightningModule(pl.LightningModule):
     def __init__(self, loss=nn.MSELoss(), optimizer={"name": "Adam", "params": {}},
-                 lr_scheduler=True, metrics=[]):
+                 lr_scheduler=True, final_activation=nn.Softmax(dim=1),
+                 metrics=[]):
         super(EnhancedLightningModule, self).__init__()
         self.loss = loss
         self.optimizer_config = optimizer
         self.lr_scheduler = lr_scheduler
         self._init_metrics(metrics)
         self.post_process = None # Have to be initalize later
+        self.final_activation = final_activation
 
 
     def _init_metrics(self, metrics):
@@ -52,8 +54,10 @@ class EnhancedLightningModule(pl.LightningModule):
             self.post_process = mtr.FillHoles(keep_labels)
         x, y = batch
         out = self.forward(x)
+        # Softmax is baked in `nn.CrossEntropyLoss`, only do it for preds
+        sout = self.final_activation(out)
         preds = []
-        for elt in out: # Post process need to be done element wise
+        for elt in sout: # Post process need to be done element wise
             preds.append(self.post_process(elt.squeeze()))
         preds = torch.stack(preds)
         return preds, self.loss(out, y)

src/networks/mixed_architecture.py
@@ -9,18 +9,19 @@ from networks.core import EnhancedLightningModule
 
 class SwinUNETR(EnhancedLightningModule):
     def __init__(self,
+                 # EnhancedLightningModule parameters
+                 loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
+                 lr_scheduler=True, final_activation=nn.Softmax(dim=1), metrics=[],
                  # Monai's SwinUNETR parameters
                  img_size=(256, 256, 256), in_channels=1, out_channels=2,
                  depths=(2, 2, 2, 2), num_heads=(3, 6, 12, 24), feature_size=24,
                  norm_name="instance", drop_rate=0, attn_drop_rate=0,
                  dropout_path_rate=0, normalize=True, use_checkpoint=False,
-                 spatial_dims=3, downsample="merging",
-                 # EnhancedLightningModule parameters
-                 loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
-                 metrics=[],
-                 # Added personal parameters
-                 final_activation=nn.Softmax(dim=1)):
-        super(SwinUNETR, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
+                 spatial_dims=3, downsample="merging"):
+        super(SwinUNETR, self).__init__(
+                loss=loss, optimizer=optimizer, lr_scheduler=lr_scheduler,
+                final_activation=final_activation, metrics=metrics
+                )
         self.model = mnn.SwinUNETR(
                 img_size, in_channels, out_channels, depths=depths,
                 num_heads=num_heads, feature_size=feature_size,
@@ -30,25 +31,25 @@ class SwinUNETR(EnhancedLightningModule):
                 use_checkpoint=use_checkpoint, spatial_dims=spatial_dims,
                 downsample=downsample
                 )
-        self.final_activation = final_activation
 
     def forward(self, x):
-        out = self.model(x)
-        return self.final_activation(out)
+        return self.model(x)
 
 class UNETR(EnhancedLightningModule):
     def __init__(self,
+                 # EnhancedLightningModule parameters
+                 loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
+                 lr_scheduler=True, final_activation=nn.Softmax(dim=1), metrics=[],
                  # Monai's UNETR parameters
                  in_channels=1, out_channels=2, img_size=(256, 256, 256),
                  feature_size=16, hidden_size=768, mlp_dim=3072, num_heads=12,
                  pos_embed="conv", norm_name="instance", conv_block=True,
                  res_block=True, dropout_rate=0, spatial_dims=3, qkv_bias=False,
-                 # EnhancedLightningModule parameters
-                 loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
-                 metrics=[],
-                 # Added personal parameters
-                 final_activation=nn.Softmax(dim=1)):
-        super(UNETR, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
+                 ):
+        super(UNETR, self).__init__(
+                loss=loss, optimizer=optimizer, lr_scheduler=lr_scheduler,
+                final_activation=final_activation, metrics=metrics
+                )
         self.model = mnn.UNETR(
                 in_channels, out_channels, img_size, feature_size=feature_size,
                 hidden_size=hidden_size, mlp_dim=mlp_dim, num_heads=num_heads,
@@ -56,9 +57,6 @@ class UNETR(EnhancedLightningModule):
                 res_block=res_block, dropout_rate=dropout_rate,
                 spatial_dims=spatial_dims, qkv_bias=qkv_bias
                 )
-        self.final_activation = final_activation
 
     def forward(self, x):
-        out = self.model(x)
-        return self.final_activation(out)
-
+        return self.model(x)

src/networks/unet.py
@@ -9,48 +9,46 @@ class BasicUNet(EnhancedLightningModule):
     def __init__(self,
                  # EnhancedLightningModule parameters
                  loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
-                 metrics=[],
+                 lr_scheduler=True, final_activation=nn.Softmax(dim=1), metrics=[],
                  # Monai's BasicUNet parameters
                  spatial_dims=3, in_channels=1, out_channels=1,
                  features=(32, 32, 64, 128, 256, 32),
                  act=("LeakyReLU", {"negative_slope": 0.1, "inplace": True}),
                  norm=("instance", {"affine": True}), bias=True, dropout=0.0,
-                 upsample="deconv",
-                 # Added personal arguments
-                 final_activation=nn.Softmax(dim=1)):
-        super(BasicUNet, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
+                 upsample="deconv"):
+        super(BasicUNet, self).__init__(
+                loss=loss, optimizer=optimizer, lr_scheduler=lr_scheduler,
+                final_activation=final_activation, metrics=metrics
+                )
         self.model = mnn.BasicUNet(
                 spatial_dims=spatial_dims, in_channels=in_channels,
                 out_channels=out_channels, features=features, act=act,
                 norm=norm, bias=bias, dropout=dropout, upsample=upsample
                 )
-        self.final_activation = final_activation
 
     def forward(self, x):
-        out = self.model(x)
-        return self.final_activation(out)
+        return self.model(x)
 
 class UNet(EnhancedLightningModule):
     def __init__(self,
                  # EnhancedLightningModule parameters
                  loss=nn.BCELoss(), optimizer={"name": "Adam", "params": {}},
-                 metrics=[],
+                 lr_scheduler=True, final_activation=nn.Softmax(dim=1), metrics=[],
                  # Monai's UNet arguments
                  spatial_dims=3, in_channels=1, out_channels=1,
                  channels=(8, 16, 32), strides=(2, 2), kernel_size=3,
                  up_kernel_size=3, num_res_units=0, act="PRELU", norm="INSTANCE",
-                 dropout=0, bias=True, adn_ordering="NDA",
-                 # Specific arguments
-                 final_activation=nn.Softmax(dim=1)):
-        super(UNet, self).__init__(loss=loss, optimizer=optimizer, metrics=metrics)
+                 dropout=0, bias=True, adn_ordering="NDA"):
+        super(UNet, self).__init__(
+                loss=loss, optimizer=optimizer, lr_scheduler=lr_scheduler,
+                final_activation=final_activation, metrics=metrics
+                )
         self.model = mnn.UNet(
                 spatial_dims, in_channels, out_channels, channels, strides,
                 kernel_size=kernel_size, up_kernel_size=up_kernel_size,
                 num_res_units=num_res_units, act=act, norm=norm,
                 dropout=dropout, bias=bias, adn_ordering=adn_ordering
                 )
-        self.final_activation = final_activation
 
     def forward(self, x):
-        out = self.model(x)
-        return self.final_activation(out)
+        return self.model(x)